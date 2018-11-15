cec6a00da6a8153586251830ccf1b278de48bae0
Andre Kastner <andre@akastner.de>
2018-11-15 17:40:33 +0100

    Updated Total

--- contributions/hibernate/org/softlang/features/Total.java
+++ contributions/hibernate/org/softlang/features/Total.java
@@ -9,26 +9,27 @@ import org.softlang.company.*;
 public class Total {
 
 	public static double total(Company company) {
-		double total = 0;
 		// total all salaries in all top departments
-		for (Department dept : company.getDepts())
-			total += total(dept);
-		return total;
+		return company.getDepts()
+				.stream()
+				.map(dept -> total(dept))
+				.reduce(0.0, (t1,t2) -> t1+t2);
 	}
 
 	public static double total(Department dept) {
-		double total = 0;
 		// total all department's employees' salaries
-		for (Employee employee : dept.getEmployees())
-			total += total(employee);
+		return dept.getEmployees()
+					.stream()
+					.map(employee -> total(employee))
+					.reduce(0.0, (t1,t2) -> t1+t2)
 		// total all salaries in all sub departments
-		for (Department subDepartment : dept.getSubdepts())
-			total += total(subDepartment);
-		return total;
+			+ dept.getSubdepts()
+					.stream()
+					.map(subDept -> total(subDept))
+					.reduce(0.0, (t1,t2) -> t1+t2);
 	}
 
 	public static double total(Employee employee) {
 		return employee.getSalary();
 	}
-
 }
